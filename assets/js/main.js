409db8bd8f179b63766130d40e6b54b821a89d10
ashwaniYDV <ashyadavash@gmail.com>
2021-09-19 00:47:00 +0530

    minor header updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,22 +44,22 @@
 			});
 
 		// Header.
-			if (skel.vars.IEVersion < 9)
-				$header.removeClass('alt');
+			// if (skel.vars.IEVersion < 9)
+			// 	$header.removeClass('alt');
 
-			if ($banner.length > 0
-			&&	$header.hasClass('alt')) {
+			// if ($banner.length > 0
+			// &&	$header.hasClass('alt')) {
 
-				$window.on('resize', function() { $window.trigger('scroll'); });
+			// 	$window.on('resize', function() { $window.trigger('scroll'); });
 
-				$banner.scrollex({
-					bottom:		$header.outerHeight(),
-					terminate:	function() { $header.removeClass('alt'); },
-					enter:		function() { $header.addClass('alt'); },
-					leave:		function() { $header.removeClass('alt'); }
-				});
+			// 	$banner.scrollex({
+			// 		bottom:		$header.outerHeight(),
+			// 		terminate:	function() { $header.removeClass('alt'); },
+			// 		enter:		function() { $header.addClass('alt'); },
+			// 		leave:		function() { $header.removeClass('alt'); }
+			// 	});
 
-			}
+			// }
 
 		// Menu.
 			var $menu = $('#menu');
