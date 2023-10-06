6e75ba4d7d23674f0f6514e6c50656895691cebc
Ashwani Yadav <ashyadavash@gmail.com>
2023-10-06 09:51:41 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/journal" element={<Journal />}></Route>
-				<Route path="/*" element={<Home />}></Route>
+				{/* <Route path="/journal" element={<Journal />}></Route> */}
+				<Route path="*" element={<Home />}></Route>
 			</Routes>
 		</BrowserRouter>
 	);
